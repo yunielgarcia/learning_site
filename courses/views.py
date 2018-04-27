84b1e519fce8d9fd3f55b73056f144b23950f117
Yuniel Garcia <garciayuni84@gmail.com>
2018-04-27 17:42:38 -0400

    working

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Course
+
+
+def course_list(request):
+    courses = Course.objects.all()
+    output = ', '.join([str(course) for course in courses])
+    return HttpResponse(output)
